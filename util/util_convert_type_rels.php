28e8156fca82d3326442f14da9202d5b88c88828
Will Stephenson <absorr@live.com>
2019-10-30 17:17:20 -0400

    Conversion utilities to load initial data

--- util/util_convert_type_rels.php
+++ util/util_convert_type_rels.php
@@ -2,12 +2,12 @@
 
 include_once '../_common/config.php';
 
-$json = json_decode(file_get_contents('ptu_pokedex_1_05.json'), true);
+$json = json_decode(file_get_contents('type-effects.json'), true);
 $sql = "INSERT INTO final_type_rel (type_name_def, type_name_atk, eff_multiplier) VALUES (:def, :atk, :multi)";
 $stmt = $database->prepare($sql);
 
-foreach (array_keys($json) as $atk) {
-    foreach ($atk as $def => $multi) {
+foreach ($json as $atk => $atkJson) {
+    foreach ($atkJson as $def => $multi) {
         $data = array(
             "atk" => $atk,
             "def" => $def,
